9635aac874bcf8740ed1336f7706da41558c8656
Saurabh More <105311140+9325993697@users.noreply.github.com>
2022-12-30 16:44:33 +0530

    Update Emp.java

--- Practical silp/Slip6/Emp.java
+++ Practical silp/Slip6/Emp.java
@@ -1,42 +1,67 @@
-import java.util.Scanner;
+import java.io.*;
+class EmpSal
+{
+    int id;
+    String name;
+    float salary;
+    public static int count=0;
 
-public class Emp {
+    EmpSal(String n,float s)
+    {
+        count++;
+        this.id=count;
+        this.name=n;
+        this.salary=s;
+    }
+    public float getSal()
+    {
+        return salary;
+    }
+    public void display()
+    {
+        System.out.println("Id->> "+id+" Name--> "+name+" Salary --> "+salary);
+    }
+    public static void sortSalary(EmpSal e[],int num)
+    {
+        for(int i=1;i<=e.length;i++)
+        {
+            for(int j=i+1;j<e.length;j++)
+            {
+                if(e[i].getSal()>e[j].getSal())
+                {
+                EmpSal t=e[i];
+                e[i]=e[j];
+                e[j]=t;
+                }
+            }
+        }
+        for(int i=0;i<num;i++)
+            e[i].display();
+    }
+    
+}
+class Employee
+{
+    public static void main(String []ar)throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter how many employees you wnat--> ");
+        int num=Integer.parseInt(br.readLine());
 
- int empid;
- String name;
- float salary;
- 
- public void getInput() {
- 
-  Scanner in = new Scanner(System.in);
-  System.out.print("Enter the empid :: ");
-  empid = in.nextInt();
-  System.out.print("Enter the name :: ");
-  name = in.next();
-  System.out.print("Enter the salary :: ");
-  salary = in.nextFloat();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
- }
- public void display(){ 
-  System.out.println("Employee id = " + empid);
-  System.out.println("Employee name = " + name);
-  System.out.println("Employee salary = " + salary);
- }
- 
- public static void main(String[] args) {
- 
-  Emp e[] = new Emp[5];
-  
-  for(int i=0; i<5; i++) {
-   
-   e[i] = new Emp();
-   e[i].getInput();
-  }
-  
-  System.out.println("**** Data Entered as below ****");
-  
-  for(int i=0; i<5; i++) {
-   
-   e[i].display();
-  }
- }
+        EmpSal s[]=new EmpSal[num];
+
+        for(int i=0;i<num;i++)
+        {
+            System.out.println("Enter Id of emp--> ");
+            int id=Integer.parseInt(br.readLine());
+            System.out.println("Enter name of emp--> ");
+            String name=br.readLine();
+            System.out.println("Enter salary of emp--> ");
+            float salary=Float.parseFloat(br.readLine());
+            
+            s[i]=new EmpSal(name, salary);
+        }
+        EmpSal.sortSalary(s, EmpSal.count);
+            
+    }
 }
